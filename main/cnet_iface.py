1d337bce0fa6d82fe6d1ab833cd26f0e2b870085
Andrew McDonald <andrewm921@gmail.com>
2013-03-05 15:46:35 -0500

    cleaned up cnet_iface, began structuring middleman.

--- main/cnet_iface.py
+++ main/cnet_iface.py
@@ -9,106 +9,108 @@ import urllib2
 import simplejson as json
 import admin
 
-log = admin.init_logger("cnet_iface", admin.LOG_MODE)
+class cnet_ifact:
 
-
-_base = "http://conceptnet5.media.mit.edu/data/5.1/"
-_lang = "en"
-
-node_type = ["concept","frame","relation","assertion","sense"]
-edge_keys = ["rel","features","startLemmas","relLemmas","endLemmas","start","end","score","weight","context","text","surfaceText","nodes","uri","sources"]
-
-'''
-
-I went home.
-
-We were eating hamburgers.
-
-He kicked the ball.
-
-The food was delicious.
-
-
-'''
-
-def similarity_check(opener,thing_one,thing_two):
-    request = _base + "assoc/c/%s/%s?filter=/c/%s/%s&limit=1" % (_lang,thing_one,_lang,thing_two)
-    page = opener.open(request)
-    return json.load(page)
-
-def associated_with(opener,concept):
-    request = _base + "assoc/list/%s/%s" % (_lang,concept)
-    page = opener.open(request)
-    return json.load(page)
-
-def concept_info(opener,concept):
-    request = _base + "c/%s/%s?filter=/c/en"% (_lang,concept)
-    page = opener.open(request)
-    return json.load(page)
-
-
-def sub_powerset(sentence_list):
-    ''' Takes a sentence as a list, and returns a sub-powerset:
-    if the sentence is: ['i', 'ate', 'a', 'delicious', 'dinner']
-    then this function will return:
-    {'1': ['i', 'ate', 'a', 'delicious', 'dinner']
-    '3': ['i ate a', 'ate a delicious', 'a delicious dinner']
-    '2': ['i ate', 'ate a', 'a delicious', 'delicious dinner']
-    '5': ['i ate a delicious dinner']
-    '4': ['i ate a delicious', 'ate a delicious dinner']}
+    _base = "http://conceptnet5.media.mit.edu/data/5.1/"
+    _lang = "en"
+    
+    node_type = ["concept","frame","relation","assertion","sense"]
+    edge_keys = ["rel","features","startLemmas","relLemmas","endLemmas","start","end","score","weight","context","text","surfaceText","nodes","uri","sources"]
+    
+    def __init__(self):  
+        self.log = admin.init_logger("cnet_iface", admin.LOG_MODE)
+        try: 
+            self.opener = urllib2.build_opener(urllib2.HTTPHandler)
+        except urllib2.HTTPError:
+            self.log.critical("Error building opener")
     '''
-    sub_powerset = {} 
-    len_list = len(sentence_list)
-    len_list_p = len_list +1
-    num_lists = (len_list*(len_list_p))/2 # we will return [m(m+1)]/2 lists, where 'm' is the number of words in the sentence.
-    start = 0
-    for offset in range(1,len_list_p):
-        start = 0
-        stop_at = len_list_p - offset
-        inner_list = []
-        for i in range(0,stop_at):
-            inner_list.append(" ".join(sentence_list[start:start+offset]))
-            start += 1
-        sub_powerset[str(offset)] = inner_list
-    print str(sub_powerset).replace("],","]\n")
-    return sub_powerset
     
-
-try: 
-    opener = urllib2.build_opener(urllib2.HTTPHandler)
-
-
+    I went home.
     
-    #result1 = similarity_check(opener,thing_one,thing_two)
-    #result2 = associated_with(opener,concept)
-    sentence = "I ate a delicious dinner."
-    sentence = sentence.strip(".?!")
-    sentence = sentence.lower()
-    print sentence
-    sentence_list = sentence.split(" ")
-    print sentence_list
-    combinations = sub_powerset(sentence_list)
-    print combinations["1"][0]
-    result3 = concept_info(opener,"eat")#combinations["1"][0])
-    result = result3
-    print result
-    try:
-        edges = result["edges"]
-        for edge in edges:
-            try:
-                print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-                for key in edge.keys():
-                    print "key: "+key
-                    print edge[key]
-                print "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-            except KeyError:
-                print "Key: '"+key+"' is not present."
-    except KeyError:
-        print "No edges returned."
+    We were eating hamburgers.
+    
+    He kicked the ball.
+    
+    The food was delicious.
+    
+    
+    '''
+        
+    def get_page(self,request):
+        '''
+        get request from ConceptNet
+        '''
+        try:
+            page = self.opener.open(request)
+            return json.load(page)
+        except urllib2.HTTPError:
+            self.log.critical("Error querying ConceptNet, could not connect!")
+            raise # do I really want to raise an exception? Maybe we should try again a few times...
+    
+    def similarity_check(self,opener,thing_one,thing_two):
+        '''
+        Use ConceptNet's association request to see how similar two concepts are.
+        '''
+        request = self._base + "assoc/c/%s/%s?filter=/c/%s/%s&limit=1" % (self._lang,thing_one,self._lang,thing_two)
+        answer = self.get_page(request)
+        try:
+            similar = answer["similar"]
+            for elem in similar:
+                self.log.info(elem)
+            return similar
+        except KeyError:
+            self.log.warning("No similarities returned for concepts: '%s' and '%s'.",thing_one,thing_two)
+            return None
+    
+    def associated_with(self,concept):
+        '''
+        Use ConceptNet to see what concepts are associated with other concepts. Also returns the 'degree' of association.
+        '''
+        request = self._base + "assoc/list/%s/%s" % (self._lang,concept)
+        answer = self.get_page(request)
+        try:
+            similar = answer["similar"]
+            for elem in similar:
+                self.log.info(elem)
+            return similar
+        except KeyError:
+            self.log.warning("No associations returned for concept: %s.",concept)
+            return None
+    
+    def concept_info(self,concept):
+        '''
+        General query for ConceptNet. Returns information regarding the input concept.
+        '''
+        request = self._base + "c/%s/%s?filter=/c/en"% (self._lang,concept)
+        answer = self.get_page(request)
+        try:
+            edges = answer["edges"]
+            for edge in edges:
+                try:
+                    for key in edge.keys():
+                        self.log.info("key: %s - %s",key,edge[key])
+                except KeyError:
+                    self.log.error("Key: '%s' is not present, but should be.",key)
+        except KeyError:
+            self.log.warning("No edges returned for concept: %s.", concept)
+    
+    
+'''     
+JUST FOR IDEAS:
 
-except urllib2.HTTPError:
-    raise
-
+        #result1 = similarity_check(opener,thing_one,thing_two)
+        #result2 = associated_with(opener,concept)
+        sentence = "I ate a delicious dinner."
+        sentence = sentence.strip(".?!")
+        sentence = sentence.lower()
+        print sentence
+        sentence_list = sentence.split(" ")
+        print sentence_list
+        combinations = sub_powerset(sentence_list)
+        print combinations["1"][0]
+        result3 = concept_info(opener,"eat")#combinations["1"][0])
+'''
+   
 class node:
     
     def __init__(self,node_type):
--- main/middleman.py
+++ main/middleman.py
@@ -7,7 +7,10 @@ middleman is the "middleman" between corenlp_iface and cnet_iface.
 '''
 import corenlp_iface
 import cnet_iface
+import admin
 from pprint import pprint
+
+log = admin.init_logger("cnet_iface", admin.LOG_MODE)
 
 class LanguageProcessor:
     
@@ -32,7 +35,50 @@ class LanguageProcessor:
         # 'PartOfSpeech' is the part of speech of the word in the sentence
         # 'Lemma' is the lemmatized version of the word. This is very important for us, because this is the version of the word that we will use when querying ConceptNet
         # The lemma may be retrieved like so: parsed["sentences"][0]["words"][0][1]["Lemma"]
-        
-        ''' get the named entity tag, part of speech, and lemma for each word. Then we can query concept net, and disregard results that aren't of the same POS. 
+        word_info_we_want = [] #list of dictionaries... one per word
+        for word_info in parsed["sentences"][0]["words"]:
+            word_info_we_want.append(word_info)
+        return word_info_we_want, parsed["sentences"][0]["parsetree"] 
+        '''Then we can query concept net, and disregard results that aren't of the same POS. 
         '''
+    
+    def query_concept_net(self,lemma_list):
+            # From "get_concept_info": Interested in "score", "weight", "context", "rel"
+            # From "associated_with": interested in 'similar' (this is a list of lists, each list has a concept and a number that is supposed to suggest the closeness of the association)
+            # From "similarity_check": Interested in 'similar'. This is the same 
+            
+            ''' TODO: set up querying concept net using the lemma's taken from 'parse' above. 
+            Determine initial keys to use from 'get_concept_info', then take the results and see how they are for each word using the similarity check.
+            '''
+            pass 
+         
+    def sub_powerset(self,sentence_list):
+        ''' Takes a sentence as a list, and returns a sub-powerset:
+        if the sentence is: ['i', 'ate', 'a', 'delicious', 'dinner']
+        then this function will return:
+        {'1': ['i', 'ate', 'a', 'delicious', 'dinner']
+        '3': ['i ate a', 'ate a delicious', 'a delicious dinner']
+        '2': ['i ate', 'ate a', 'a delicious', 'delicious dinner']
+        '5': ['i ate a delicious dinner']
+        '4': ['i ate a delicious', 'ate a delicious dinner']}
+        '''
+        sub_powerset = {} 
+        len_list = len(sentence_list)
+        len_list_p = len_list +1
+        num_lists = (len_list*(len_list_p))/2 # we will return [m(m+1)]/2 lists, where 'm' is the number of words in the sentence.
+        start = 0
+        for offset in range(1,len_list_p):
+            start = 0
+            stop_at = len_list_p - offset
+            inner_list = []
+            for i in range(0,stop_at):
+                inner_list.append(" ".join(sentence_list[start:start+offset]))
+                start += 1
+            sub_powerset[str(offset)] = inner_list
+        print str(sub_powerset).replace("],","]\n")
+        return sub_powerset
+        
+    
+            
+        
         
